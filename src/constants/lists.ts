55be8cd84b0399aefe991a3cdcadd24db700c567
Muhammad Shafi <shafimalik63@gmail.com>
2024-05-02 11:15:36 +0500

    refactor: update compound token list json file url

--- src/constants/lists.ts
+++ src/constants/lists.ts
@@ -1,4 +1,4 @@
-const COMPOUND_LIST = 'https://raw.githubusercontent.com/hassan-webtech/compound-list/main/compound-token-list.json';
+const COMPOUND_LIST = 'https://raw.githubusercontent.com/zOthix/compound-list/btc/compound-token-list.json';
 
 // lower index == higher priority for token import
 export const DEFAULT_LIST_OF_LISTS: string[] = [COMPOUND_LIST];
